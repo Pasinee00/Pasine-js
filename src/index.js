f114ca0f2e79ababda0036130b3b301ed424aaa6
Pasinee00 <59599220+Pasinee00@users.noreply.github.com>
2020-11-23 10:23:40 +0700

    test arrow function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-let nickname = "your nickname";
-console.log(nickname);
+//function add(a, b) {
+// return a + b;
+//}
+const add = (a, b) => a + b;
+
+// const add = (a, b) => {
+//   return a + b;
+// }
+
+// const add = (a, b) => a + b
+
+console.log(add(1, 2));
